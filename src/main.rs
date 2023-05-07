918c2f5d0e7d0de2391ede0de1e467f0f5751475
Daniel <0243179@up.edu.mx>
2023-05-07 00:20:33 -0600

    basic func for queriis

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,55 @@
-#![recursion_limit = "256"]
+use anyhow::{anyhow, Error, Result};
 use leptos::*;
+use sqlx::mysql::MySqlPool;
+use sqlx::{Connection, MySql};
+use std::any::Any;
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+use std::time::Duration;
+use tokio::time::error::Elapsed;
 
-mod schema;
-use diesel::*;
-use schema::*;
-
-#[derive(Queryable, PartialEq, Debug, Selectable)]
-#[diesel(table_name = accidentes_2018)]
-struct IdsAcc {
-    id: i32,
-    ID_ENTIDAD: String,
+async fn conection() -> MySqlPool {
+    MySqlPool::connect("msyql://root:1234@localhost/Accidents")
+        .await
+        .unwrap()
 }
 
-pub fn establish_connection() -> MysqlConnection {
-    MysqlConnection::establish("mysql://root:1234@localhost/Accidents").unwrap()
+pub async fn make_query<T>(query: impl AsRef<str>, connection: &sqlx::Pool<MySql>) -> Result<Vec<T>>
+where
+    for<'a> T: sqlx::FromRow<'a, sqlx::mysql::MySqlRow> + Send + Unpin,
+{
+    let result: Vec<T> = sqlx::query_as::<_, T>(query.as_ref())
+        .fetch_all(connection)
+        .await?;
+
+    if result.is_empty() {
+        Err(anyhow!("No se encontró ningún dato"))
+    } else {
+        Ok(result)
+    }
+}
+
+pub async fn make_query_expect_empty<T>(
+    query: impl AsRef<str>,
+    connection: &sqlx::Pool<MySql>,
+) -> Result<Vec<T>>
+where
+    for<'a> T: sqlx::FromRow<'a, sqlx::mysql::MySqlRow> + Send + Unpin,
+{
+    let result: Vec<T> = sqlx::query_as::<_, T>(query.as_ref())
+        .fetch_all(connection)
+        .await?;
+
+    if !result.is_empty() {
+        Err(anyhow!("Se encontró uno o más datos"))
+    } else {
+        Ok(result)
+    }
 }
 
 fn main() {
-    use schema::accidentes_2018::dsl::*;
+    let pool = conection();
 
-    let mut conection = establish_connection();
-    let prueba = accidentes_2018
-        .select((id, ID_ENTIDAD))
-        .load::<IdsAcc>(&mut conection);
-    println!("{:?}", prueba);
+    println!("hola");
 }
